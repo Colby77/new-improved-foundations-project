9ad7cc6ddbb45a40659c2c91e3deade3734cb5d6
Colby77 <cgw2319@gmail.com>
2023-01-20 12:10:58 -0800

    initial commit

---
+++ backend/main.py
@@ -0,0 +1,17 @@
+"""
+The main file for the FastAPI backend
+
+Run the server with:
+    'uvicorn main:app --reload'
+"""
+
+from fastapi import FastAPI
+
+
+app = FastAPI()
+
+
+@app.get("/")
+def root():
+    return {"Hello": "World"}
+
