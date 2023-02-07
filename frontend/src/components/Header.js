399193a4399511eb18b949ba72710e05635f73bb
Colby77 <cgw2319@gmail.com>
2023-02-07 13:47:08 -0800

    more details added to Header

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,6 +1,20 @@
 const Header = () => {
   return (
-    <header>This is the header</header>
+    <header>
+        <img alt='wizard-logo'></img>
+        <nav>
+            <ul>
+                <li><a href='/'>Home</a></li>
+                <li><a href='/create-char'>Create Character</a></li>
+                <li><a>Shop</a></li>
+                <li><a>About</a></li>
+            </ul>
+        </nav>
+        <div>
+            <img alt='cart'></img>
+            <h4>Account</h4>
+        </div>
+    </header>
   )
 }
 
