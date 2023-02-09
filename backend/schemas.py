8c35788d70f89235791d868013250cf1a8bb877a
Colby77 <cgw2319@gmail.com>
2023-02-09 15:43:13 -0800

    pydantic models/schemas added

---
+++ backend/schemas.py
@@ -0,0 +1,44 @@
+from pydantic import BaseModel
+
+
+class CharacterBase(BaseModel):
+    is_private: bool
+    name: str
+    race: str
+    char_class: str
+    background: str
+    strength: int
+    dexterity: int
+    constitution: int
+    wisdom: int
+    intelligence: int
+    charisma: int
+    skills: str
+    languages: str
+    alignment: str
+
+class Character(CharacterBase):
+    id: int
+    creator_id: int
+
+    class Config:
+        orm_mode = True
+
+class CharacterCreate(CharacterBase):
+    pass
+
+
+class UserBase(BaseModel):
+    email: str
+
+class UserCreate(UserBase):
+    password: str
+
+class User(UserBase):
+    id: int
+    username: str
+    characters: list[Character] = []
+
+    class Config:
+        orm_mode = True
+
