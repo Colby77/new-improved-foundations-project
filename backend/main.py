333a63a4a3a0a1da34127c6c91ccb8727c72bd49
Colby77 <cgw2319@gmail.com>
2023-02-10 13:30:04 -0800

    path operations for db added

--- backend/main.py
+++ backend/main.py
@@ -5,10 +5,25 @@ Run the server with:
     'uvicorn main:app --reload'
 """
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy.orm import Session
 
+from . import crud, models, schemas
+from .database import SessionLocal, engine
+
+
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+
+# Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 
 @app.get("/")
@@ -19,3 +34,37 @@ def root():
             }
         }
 
+
+@app.post('/users/', response_model=schemas.User)
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    db_user = crud.get_user_by_email(db, email=user.email)
+    if db_user:
+        raise HTTPException(status_code=400, detail='Email already registered')
+    return crud.create_user(db=db, user=user)
+
+
+@app.get('/users/', response_model=list[schemas.User])
+def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    users = crud.get_users(db, skip=skip, limit=limit)
+    return users
+
+
+@app.get('/users/{user_id}', response_model=schemas.User)
+def read_user(user_id: int, db: Session = Depends(get_db)):
+    db_user = crud.get_user(db, user_id=user_id)
+    if db_user is None:
+        raise HTTPException(status_code=404, detail='User not found')
+    return db_user
+
+
+@app.post('/users/{user_id}/characters/', response_model=schemas.Character)
+def create_character_for_user(
+    user_id: int, character: schemas.CharacterCreate, db: Session = Depends(get_db)
+    ):
+    return crud.create_character(db=db, character=character, user_id=user_id)
+
+
+@app.get('/characters', response_model=list[schemas.Character])
+def read_characters(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    characters = crud.get_characters(db, skip=skip, limit=limit)
+    return characters
