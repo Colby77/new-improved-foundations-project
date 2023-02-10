850e4279474674b83f03bdbd2908181744d4e0d0
Colby77 <cgw2319@gmail.com>
2023-02-09 16:07:43 -0800

    crud operations added

---
+++ backend/crud.py
@@ -0,0 +1,36 @@
+from sqlalchemy.orm import Session
+
+from . import models, schemas
+
+
+def get_user(db: Session, user_id: int):
+    return db.query(models.User).filter(models.User.id == user_id).first()
+
+
+def get_user_by_email(db: Session, email: str):
+    return db.query(models.User).filter(models.User.email == email).first()
+
+
+def get_users(db: Session, skip: int=0, limit: int = 100):
+    return db.query(models.User).offset(skip).limit(limit).all()
+
+
+def create_user(db: Session, user: schemas.UserCreate):
+    fake_hashed_password = user.password + 'fakehashedpassword'
+    db_user = models.User(email=user.email, hashed_password=fake_hashed_password)
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
+
+
+def get_characters(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(models.Character).offset(skip).limit(limit).all()
+
+
+def create_character(db: Session, character: schemas.CharacterCreate, user_id: int):
+    db_character = models.Character(**character.dict(), creator=user_id)
+    db.add(db_character)
+    db.commit()
+    db.refresh(db_character)
+    return db_character
