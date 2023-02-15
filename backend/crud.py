4271bbd7e250af4583c077a1c1c6ecee8972396c
Colby77 <cgw2319@gmail.com>
2023-02-15 12:48:29 -0800

    fix for password requirement when registering users

--- backend/crud.py
+++ backend/crud.py
@@ -17,7 +17,7 @@ def get_users(db: Session, skip: int=0, limit: int = 100):
 
 def create_user(db: Session, user: schemas.UserCreate):
     fake_hashed_password = user.password + 'fakehashedpassword'
-    db_user = models.User(email=user.email, hashed_password=fake_hashed_password)
+    db_user = models.User(email=user.email, username=user.username, hashed_password=fake_hashed_password)
     db.add(db_user)
     db.commit()
     db.refresh(db_user)
